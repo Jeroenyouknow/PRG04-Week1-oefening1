189db7df45bc5097318af8513872386ba7849766
Jeroen Huiskes <jeroen-h@hotmail.nl>
2017-05-07 20:28:34 +0200

    Version 1.0

--- js/index.js
+++ js/index.js
@@ -1,29 +1,36 @@
 // start when the document is loaded
 console.log("starting...");
 
-function init() {
-  // create data
+// create data
   let wordsStart = "Dum,Dam,Da,Dum,Da,Da,Da,Dum,Thats,The,Way,You've,Got,To,GO!";
+  let arrayStart = wordsStart.split(",");
   let wordsEnd = "No,No,No,No,No,No,No,No,No,No,No,No,There's,No,Limit";  
+  let arrayEnd = wordsEnd.split(",");
 
-  buildButtons();
+window.addEventListener("load", init);  
+
+
+function init() {
+   buildButtons();
 }
 
 // build the row of buttons. each button has one label from the list of words
 function buildButtons() {
-  for (let i = 0; i < wordsStart.length; i++) {
-    let button = createButton();
+  for (let i = 0; i < arrayStart; i++) {
+    let button = createButton(arrayStart[i], arrayEnd[i]);
   }
 }
 
 // build one button that changes its label when clicked
-function createButton() {
+function createButton(start, end) {
   let button = document.createElement("button");
-  button.innerHTML = "start word here";
-  button.endText = "end word here";
+  button.innerHTML = start;
+  button.endText = end;
+
+  
 
   button.addEventListener("click", function(e) {
-    e.target.innerHTML = "new word here";
+    e.target.innerHTML = this.endText;
   });
   
   document.body.appendChild(button);
